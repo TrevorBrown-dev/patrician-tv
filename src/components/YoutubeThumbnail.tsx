077d1b07e8ee6e6090244bd2a03dde418590c88f
Trevor Brown <trevorbrown-dev@github.com>
2021-09-13 17:24:07 -0400

    cleaned up some more MQs

--- src/components/YoutubeThumbnail.tsx
+++ src/components/YoutubeThumbnail.tsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { SpotlightContext } from "../contexts/spotlightContext";
+import { useContext } from 'react';
+import { SpotlightContext } from '../contexts/spotlightContext';
 
 interface YoutubeThumbnailProps {
     src: string;
@@ -10,28 +10,30 @@ interface YoutubeThumbnailProps {
 export const renderSkeletons = (num: number): JSX.Element[] => {
     const skeletons = [];
     for (let i = 0; i < num; i++) {
-        skeletons.push(<YoutubeThumbnailSkeleton />);
+        skeletons.push(<YoutubeThumbnailSkeleton key={i} />);
     }
     return skeletons;
-}
+};
 export const YoutubeThumbnailSkeleton: React.FC = () => {
     return (
-        <div className="youtube-thumbnail">
-            <div className="image-placeholder"></div>
-            <div className="title-placeholder"></div>
+        <div className='youtube-thumbnail'>
+            <div className='image-placeholder'></div>
+            <div className='title-placeholder'></div>
         </div>
     );
-}
+};
 
 export const YoutubeThumbnail: React.FC<YoutubeThumbnailProps> = ({ src, alt, title, videoId }) => {
     const spotlightState = useContext(SpotlightContext);
     const handleThumbnailClick = () => {
         spotlightState?.setSpotlight({ videoId, title });
-    }
+    };
     return (
-        <div className="youtube-thumbnail" onClick={e => handleThumbnailClick()}>
-            <img src={src} alt={alt} className="image" />
-            <a href={`https://www.youtube.com/watch?v=${videoId}`} onClick={(e) => e.stopPropagation()} target="_blank" rel="noopener noreferrer" ><div className="title">{title}</div></a>
+        <div className='youtube-thumbnail' onClick={(e) => handleThumbnailClick()}>
+            <img src={src} alt={alt} className='image' />
+            <a href={`https://www.youtube.com/watch?v=${videoId}`} onClick={(e) => e.stopPropagation()} target='_blank' rel='noopener noreferrer'>
+                <div className='title'>{title}</div>
+            </a>
         </div>
     );
-}
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,10 @@
-import { AllAnalyses } from "../components/AllAnalyses";
+import { AllAnalyses } from '../components/AllAnalyses';
 
 export const Home: React.FC = () => {
-
     return (
         <main>
-            <h2 style={{ textAlign: "center" }}>New to the Channel? Start Here!</h2>
+            <h2 className='call-to-action'>New to the Channel? Start Here!</h2>
             <AllAnalyses />
         </main>
     );
-}
+};
