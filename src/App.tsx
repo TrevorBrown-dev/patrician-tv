da7c65b135024aa014e03aeee0ca80cdfb5305f9
Trevor Brown <trevorbrown.dev@gmail.com>
2021-09-13 12:40:18 -0400

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import './styles/style.css'
 import { Header } from './components/Header/Header';
 import { Navbar } from './components/Navbar';
 import { Home } from './pages/Home';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './styles/style.css'
 import { Spotlight } from './pages/Spotlight';
 import { TheCult } from './pages/TheCult';
 import { Footer } from './components/Footer';
