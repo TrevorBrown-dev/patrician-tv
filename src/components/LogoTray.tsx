e2c745ba428bbce496fa704a70905d08c107e873
Trevor Brown <trevorbrown.dev@gmail.com>
2021-09-13 14:08:36 -0400

    added twitter

--- src/components/LogoTray.tsx
+++ src/components/LogoTray.tsx
@@ -1,5 +1,6 @@
 import { PatreonLogo } from "./svg/PatreonLogo";
 import { SubscribeStar } from "./svg/SubscribeStar";
+import { TwitterLogo } from "./svg/TwitterLogo";
 import { YoutubeLogo } from "./svg/YoutubeLogo";
 
 export const LogoTray: React.FC = () => {
@@ -7,6 +8,9 @@ export const LogoTray: React.FC = () => {
         <div className="logo-tray">
             <a className="logo" title="YouTube" href="https://www.youtube.com/c/PatricianTV">
                 <YoutubeLogo />
+            </a>
+            <a className="logo" title="YouTube" href="https://twitter.com/patrician_tv">
+                <TwitterLogo />
             </a>
             <a className="logo" title="Patreon" href="https://www.patreon.com/patricianTV">
                 <PatreonLogo />
