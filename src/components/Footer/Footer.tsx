1c80483326d013782c9e156503b29a7138860603
Trevor Brown <trevorbrown-dev@github.com>
2021-09-13 17:05:40 -0400

    added some media queries

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from 'react';
+
 export const Footer: React.FC = () => {
+    useEffect(() => {
+        const texts = ['This footer is highly edited.', 'Not an Elder Scrolls channel.'];
+        setJokeText(texts[Math.floor(Math.random() * texts.length)]);
+    }, []);
+    const [jokeText, setJokeText] = useState('This footer is highly edited.');
     return (
-        <footer className="footer">
-            This footer is highly edited.
+        <footer className='footer'>
+            <div className='joke-of-the-day'>{jokeText}</div>
+            <div id='copyright'>&copy; Patrician &mdash; All rights reserved.</div>
         </footer>
-    )
-}
+    );
+};
