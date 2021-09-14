cd2efdf00cc99519a9940ee4e857b9c5ea141980
Trevor Brown <trevorbrown.dev@gmail.com>
2021-09-14 15:46:33 -0400

    Sites done pretty much

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,19 @@ import { Header } from './components/Header/Header';
 import { NavbarHorizontal, NavbarVertical } from './components/Navbar';
 import { Home } from './pages/Home';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Spotlight } from './pages/Spotlight';
-import { TheCult } from './pages/TheCult';
 import { Footer } from './components/Footer';
 import { SpotlightContext, SpotlightProps } from './contexts/spotlightContext';
 import { useEffect, useState } from 'react';
-import { getMostRecentVideo } from './components/MostRecentVideo';
+import { useMostRecentVideo } from './components/MostRecentVideo';
 const PrivateApp: React.FC = () => {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Header />
-      <NavbarHorizontal />
       <div className="page-main">
         <aside className="sidebar">
-          <NavbarVertical />
         </aside>
-        <div className="content">
+        <div className="content" style={{ gridColumn: '1/-1' }}>
           <Route path="/" component={Home} exact />
-          <Route path="/spotlight" component={Spotlight} exact />
-          <Route path="/thecult" component={TheCult} exact />
-
         </div>
       </div>
       <Footer />
@@ -32,11 +25,10 @@ const PrivateApp: React.FC = () => {
 
 export const App: React.FC = () => {
   const [spotlight, setSpotlight] = useState<SpotlightProps | null>({ videoId: "", title: "" });
-  useEffect(() => {
-    getMostRecentVideo().then(payload => {
-      setSpotlight(payload);
-    })
-  }, [])
+  useMostRecentVideo().then(payload => {
+    setSpotlight(payload);
+  });
+
   return (
     <SpotlightContext.Provider value={{ spotlight, setSpotlight }}>
       <PrivateApp />
